694242544b384f426747bfd10157008745617e97
Niall Mullally <niall@4l.ie>
2010-04-15 09:35:15 +0100

    Added some simple unit tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,25 @@
 require 'rubygems'
+require 'test/unit'
 require 'active_support'
 require 'active_support/test_case'
+require File.dirname(__FILE__) + '/../lib/test_unit_exclusion_list'
+
+class MockTester
+  
+  attr_accessor :method_name
+  
+  def initialize(name)
+    self.method_name = name
+  end
+  
+end
+
+class MockTesterSubclass < MockTester
+  
+  acts_as_test_exclusion_list
+  
+  def exclusion_list
+    %w(test_to_be_excluded)
+  end
+  
+end
--- test/test_unit_exclusion_list_test.rb
+++ test/test_unit_exclusion_list_test.rb
@@ -1,8 +1,15 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/test_helper'
 
 class TestUnitExclusionListTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "Method in exclusion list is renamed" do
+    tester = MockTesterSubclass.new("test_to_be_excluded")
+    assert_equal "skip_test", tester.method_name
   end
+  
+  test "Method not in exclusion list is not renamed" do
+    tester = MockTesterSubclass.new("test_not_to_be_excluded")
+    assert_equal "test_not_to_be_excluded", tester.method_name
+  end
+
 end
